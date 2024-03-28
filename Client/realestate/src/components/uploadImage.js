ac2ef360bc153c23616fb3fbb77e2cc03c2fd69a
Shira-Abrams <sh3000444@gmail.com>
2024-03-29 00:35:08 +0200

    list of picture succed

--- Client/realestate/src/components/uploadImage.js
+++ Client/realestate/src/components/uploadImage.js
@@ -4,29 +4,54 @@ import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 export default function UploadImage() {
-    const[imagesUrl,setImagesUrl]=useState()
+    const[imagesUrl,setImagesUrl]=useState([])
 
-    const setImage=(e)=>{
-        let image=e.target.files[0]
+    const setImage=(event)=>{
+        let images=event;
+        const url=[];
+        for (let i = 0; i < images.length; i++) {
+            const image=images[i]
+            const reader = new FileReader();
+            reader.onload = () => {
+            const imurl=reader.result;
+
+                if(!url.includes(imurl))
+                 url.push(imurl)
+                if(url.length===images.length)
+                 {
+                    setImagesUrl(prevImage=>[...prevImage,...url]);
+                    console.log(url.length);
+                    console.log(imagesUrl);
+                 }  
+                // Set the image source once the file is loaded
+                
+            };
+         reader.readAsDataURL(image);     
+        }    
+            
+       // Read the file as a data URL
+   
+       console.log(imagesUrl);
+
        
+       
+
+
         
-        const reader = new FileReader();
-        reader.onload = () => {
-            // Set the image source once the file is loaded
-            setImagesUrl(reader.result);
-        };
-        if (image) {
-            reader.readAsDataURL(image); // Read the file as a data URL
-          }
+       
     }
   return (
     <div>
-        <input type='file' onChange={setImage}>
-        </input>
+        <input type='file' onChange={(e)=>setImage(e.target.files)} accept="image/*" multiple  />
          <img src={imagesUrl}/>
         {/* {imagesUrl.map((item,index)=>{
             <img src={imagesUrl[index]}/>
         })} */}
+        {imagesUrl.map((url,index)=>(
+            <div key={index}>
+                 <img src={url} style={{width:'30vh'}} alt={`Uploaded ${index}`} />
+            </div>
+        ))}
         
     </div>
   )
